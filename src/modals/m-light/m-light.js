e3cca07a3fd992a636ba73b674e9ac671f3ef7de
0x6368656174 <0x6368656174@gmail.com>
2018-08-07 00:19:15 +1000

    light range

--- src/modals/m-light/m-light.js
+++ src/modals/m-light/m-light.js
@@ -1,8 +1,22 @@
 import {Modal} from '../modal';
+import ResizeObserver from 'resize-observer-polyfill';
 
 class MLight extends Modal {
   constructor() {
     super('m-light');
+
+    const rangeContainer = this._modal.querySelector('.m-light__range-container');
+    const range = this._modal.querySelector('.m-light__range');
+    const ro = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const {height} = entry.contentRect;
+
+        range.style.width = `${height - 50}px`;
+        range.style['min-width'] = `${height - 50}px`;
+      }
+    });
+
+    ro.observe(rangeContainer);
   }
 }
 
